2fe47f689628652032312401d2cac86e91040665
Anupam Rajput <132653678+anupam9935@users.noreply.github.com>
2024-09-04 22:59:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,54 +1,13 @@
-const express=require("express");
-const cors=require("cors");
-const mongoose=require("mongoose");
-const userRoutes=require("./routes/userRoutes");
-const messageRoute=require("./routes/messagesRoute");
-const socket = require ("socket.io");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 
-const app=express();
-require('dotenv').config({ path: '../.env' });
-
-app.use(cors());
-app.use(express.json());
-app.use("/api/auth",userRoutes);
-app.use("/api/messages",messageRoute);
-const host = process.env.REACT_APP_HOST;
-//console.log(host);
-
-mongoose.connect(process.env.MONGO_URL)
-
-  .then(() => {
-    console.log("Mongoose connected");
-  })
-  .catch((error) => {
-    console.log("Mongoose not connected", error);
-  });
-
-
-const server=app.listen(process.env.PORT,()=>{
-    console.log(`server started on port ${process.env.PORT}`);
-})
-
-const io=socket(server,{
-    cors:{
-        origin:process.env.ORIGIN,
-        credentials:true,
-    },
-})
-global.onlineUsers=new Map();
-
-io.on("connection",(socket)=>{
-    global.chatSocket=socket;
-    socket.on("add-user",(userId)=>{
-          onlineUsers.set(userId,socket.id);
-    });
-    socket.on("send-msg",(data)=>{
-        const sendUserSocket=onlineUsers.get(data.to);
-        if(sendUserSocket){
-            socket.to(sendUserSocket).emit("msg-receive",data.message);
-        }
-    });
-})
-
- module.exports = host;
